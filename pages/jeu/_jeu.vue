a718e5b9c30674f07734904e8889ba2ccc4fd56a
Alexandre Ferreira <35708209+UnknownChick@users.noreply.github.com>
2021-03-31 21:14:11 +0200

    Add files via upload

---
+++ pages/jeu/_jeu.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="content">
+        <h1>{{jeux.post_title}}</h1>
+
+        <p>{{jeux.post_content}}</p>
+
+        <!-- .replace(/<(.+?)>/g, ' ').replace(/ {2,}/g, ' ') Regex ne fonctionne pas-->
+    </div>
+    
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return{
+            jeux: []
+        }
+    },
+    mounted() {
+        let nbr = this.$route.params.jeu;
+        console.log(this.$route.params);
+        axios.get('http://sitetest.local/wp-json/wp/v2/jeux_video').then(response => {
+            this.jeux = response.data[nbr];console.log(this.jeux)
+            });
+        console.log(nbr);
+    }
+}
+</script>
+
+<style>
+
+    h1{
+        padding-top: 2rem;
+        display: flex;
+        justify-content: center;
+    }
+
+</style>
